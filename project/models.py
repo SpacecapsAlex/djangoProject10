d0f100ce12598169f1562d03ec74e3daf2096a2c
SpacecapsAlex <spacecapsalex@hotmail.com>
2023-12-21 19:00:57 +0300

    Add Project model to models.py

--- project/models.py
+++ project/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    email = models.EmailField(max_length=254)
+    countUser = models.IntegerField(default=0)
