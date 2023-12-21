65a9a6a2142f948c976d54a0df6059790bf6428b
SpacecapsAlex <spacecapsalex@hotmail.com>
2023-12-21 19:38:06 +0300

    Add view and template for listing all projects

---
+++ project/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import get_projects
+
+urlpatterns = [
+    path('', get_projects),
+]
--- project/views.py
+++ project/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+from .models import Project
+
+
 # Create your views here.
+
+
+def get_projects(request: HttpRequest) -> HttpResponse:
+    projects = Project.objects.all()
+    return render(
+        request,
+        'project/GetAllProjects.html',
+        {'projects': projects}
+    )
